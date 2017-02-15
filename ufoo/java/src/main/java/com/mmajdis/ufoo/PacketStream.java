b5b1dca76f0a63d2f4494ef3fce99b93c7ec8bfd
Matej Majdis <mato.majdis@gmail.com>
2017-02-16 00:36:30 +0100

    footprint impl - java - tcp for win

--- ufoo/java/src/main/java/com/mmajdis/ufoo/PacketStream.java
+++ ufoo/java/src/main/java/com/mmajdis/ufoo/PacketStream.java
@@ -1,5 +1,7 @@
 package com.mmajdis.ufoo;
 
+import org.apache.commons.lang.SystemUtils;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -13,11 +15,17 @@ public class PacketStream implements Runnable {
      * Main startup method
      */
     public void start() {
-        String[] cmd = {"/bin/bash", "-c", "tcpdump -e -U -n -ieth0 \"tcp\" -P in -S -tt -v"};
+        String[] cmdLinux = {"/bin/bash", "-c", "tcpdump -e -U -n -ieth0 \"tcp\" -P in -S -tt -v"};
+        String cmdWindows = "cmd /c tcpdump -i \"\\Device\\NPF_{D705A902-0D2E-4121-A88B-E7A6C582EAE7}\"  -e -U -n -S -tt -v";
+
         Process pb = null;
 
         try {
-            pb = Runtime.getRuntime().exec(cmd);
+           if(SystemUtils.IS_OS_WINDOWS){
+               pb = Runtime.getRuntime().exec(cmdWindows);
+           } else {
+               pb = Runtime.getRuntime().exec(cmdLinux);
+           }
 
             String line;
             BufferedReader input = new BufferedReader(new InputStreamReader(pb.getInputStream()));
