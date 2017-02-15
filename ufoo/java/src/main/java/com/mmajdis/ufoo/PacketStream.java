bdd50bb9c5f370599597264120f6025d4c1b5bda
Matej Majdis <mato.majdis@gmail.com>
2017-02-15 16:36:08 +0100

    footprint impl - java - tcpdump test

--- ufoo/java/src/main/java/com/mmajdis/ufoo/PacketStream.java
+++ ufoo/java/src/main/java/com/mmajdis/ufoo/PacketStream.java
@@ -1,5 +1,9 @@
 package com.mmajdis.ufoo;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * @author Matej Majdis
  */
@@ -7,9 +11,23 @@ public class PacketStream implements Runnable {
 
     /**
      * Main startup method
-     *
      */
     public void start() {
+        String[] cmd = {"/bin/bash", "-c", "tcpdump -e -U -n -ieth0 \"tcp\" -P in -S -tt -v"};
+        Process pb = null;
+
+        try {
+            pb = Runtime.getRuntime().exec(cmd);
+
+            String line;
+            BufferedReader input = new BufferedReader(new InputStreamReader(pb.getInputStream()));
+            while ((line = input.readLine()) != null) {
+                System.out.println(line);
+            }
+            input.close();
+        } catch (IOException e) {
+            return;
+        }
     }
 
 
