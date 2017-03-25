8df7ab9fa64f4cb6716b9ecb946d76b7216c8126
Matej Majdis <mato.majdis@gmail.com>
2017-03-25 03:23:51 +0100

    footprint impl - java - similarity search algorithm - pseudo

--- ufoo/similarity.pseudo-code.c
+++ ufoo/similarity.pseudo-code.c
@@ -1,47 +1,60 @@
-getNearestneigbour(uFooEntity) {
 
-    staticData, relationData = deserializeUFooEntity(uFooEntity);
+algorithm get-nearest-neigbour is
+    input: UFooEntity uFooEntity
+           UFooEntity[] allStockItems
+    output: Result<int distance, UFooEntity nearestNeigbour>
 
-    minDistance = 2;
-    nearestneigbour = null;
+    minDistance ← 2;
+    nearestneigbour ← null;
 
-    for(stockUFooEntity in allStockItems) {
+    for each stockUFooEntity in allStockItems do
 
-        acctualDistance = getDistance(staticData, stockUFooEntity.staticData);
+        acctualDistance ← getDistance(uFooEntity.staticData, stockUFooEntity.staticData);
 
-        // Addition analysis - manual checks based on Relation Data
-        // Possibly can SLIGHTLY afffect distance 
+        # Addition analysis - manual checks based on Relation Data
+        # Possibly can SLIGHTLY afffect distance 
 
-        if(minDistance < acctualDistance) {
-            minDistance = acctualDistance;
-            nearestneigbour = stockUFooEntity;
-        }
-    }
+        if minDistance < acctualDistance then
+            minDistance ← acctualDistance;
+            nearestneigbour ← stockUFooEntity;
+        end if
+    
 
     return Result<minDistance, nearestneigbour>
-}
 
-getDistance(actualData, stockEntityData) {
 
-    if(actualData.equals(stockEntityData)) {
-        return 0;
-    }
 
-    //compute Jaccard and weight for static headers
-    double jaccardStaticHeaders = jaccardForSet(actualData.staticHeaders, stockEntityData.staticHeaders);
-    double jaccardStaticWeight = jaccardStaticHeaders * SH_WEIGHT;
+algorithm get-distance is
+    input: String actualData
+           String stockEntityData
+    output: double distance
 
-    //compute Jaccard for unknown headers
-    double jaccardUnknownHeaders = jaccardForSet(actualData.unknownHeaders, stockEntityData.unknownHeaders);
-    double jaccardUnknownWeight = jaccardUnknownHeaders * UH_WEIGHT;
+    if actualData = stockEntityData
+        return 0
+    end if
 
-    int isSameIp = similarValue(actaulData.IP, stockEntityData.IP);
-    int isSameCountry = similarValue(actaulData.country, stockEntityData.country);
+    # Compute Jaccard and consider weight (strength) for static headers
+    jaccardStaticHeaders ← jaccardIndex(actualData.staticHeaders, stockEntityData.staticHeaders);
+    jaccardStaticHeaders ← jaccardStaticHeaders * SH_WEIGHT;
+
+    # Compute Jaccard and consider weight (strength) for unknown headers
+    jaccardUnknownHeaders ← jaccardIndex(actualData.unknownHeaders, stockEntityData.unknownHeaders);
+    jaccardUnknownHeaders ← jaccardUnknownHeaders * UH_WEIGHT;
+
+    # Compare values of remaining attributes by simple similarValue function
+    # Every of this attributes will have sub-weight which will be computed in attributesIndex
+    isSameIp ← similarValue(actaulData.IP, stockEntityData.IP) * IP_SUB_WEIGHT;
+    isSameCountry ← similarValue(actaulData.country, stockEntityData.country) * COUNTRY_SUB_WEIGHT;
     ...
-    int isSameLength = similarValue(actaulData.length, stockEntityData.length);
-    double infoWeigth = ((isSameIp * IP_SUB_WEIGHT + isSameCountry * COUNTRY_SUB_WEIGHT + ... + isSameLength * LENGTH_SUB_WEIGHT) / SUB_WEIGHT_SUM) * INFO_WEIGHT;
+    isSameLength ← similarValue(actaulData.length, stockEntityData.length) * LENGTH_SUB_WEIGHT;
 
-    double similarity = jaccardStaticWeight + jaccardUnknownWeight + infoWeigth / WEIGHT_SUM;
+    # AttributesIndex based on SUM of weighted attributes is computed
+    # Additional weight for all attributes is applied
+    attributesIndex ← ∑(isSameIp, isSameCountry, ..., isSameLength) / ∑(IP_SUB_WEIGHT, COUNTRY_SUB_WEIGHT, ..., LENGTH_SUB_WEIGHT);
+    attributesIndex ← attributesIndex * ATTR_WEIGHT
+
+    # Final similarity value is computed as sum of weighted: staticHeaders, unknownHeaders and attributes
+    # Distance as 1 - sim value is returned
+    similarity ← ∑(jaccardStaticHeaders,  jaccardUnknownHeaders, attributesIndex) / ∑(SH_WEIGHT, UH_WEIGHT, ATTR_WEIGHT)
 
     return 1 - similarity;
-}
